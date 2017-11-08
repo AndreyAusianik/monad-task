9c87267927bf4526a17e8eacd4641b433ccff2bb
Andrei Ausianik <andrei.ausianik@iii.com>
2017-11-08 12:45:38 +0300

    can reach task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const matrix8x8 = mapFn =>
+    new Array(8).fill(0).map((_, i) => new Array(8).fill(0).map((_, j) => mapFn(i, j)));
+
+const at = matrix => (i, j) => i >= 0 && i < matrix.length && j >= 0 && j < matrix[0].length && matrix[i][j];
+
 module.exports = function canReach(startPos, finalPos, steps) {
-  // your solution here
-}
+  let imIn = matrix8x8((i, j) => i === startPos[0] - 1 && j === startPos[1] - 1);
+  for(let i = 0; i < steps; i++) {
+      const get = at(imIn);
+      imIn = matrix8x8((i, j) => get(i - 1, j - 2) || get(i - 1, j + 2) || get(i + 1, j - 2) || get(i + 1, j + 2) ||
+          get(i - 2, j - 1) || get(i - 2, j + 1) || get(i + 2, j - 1) || get(i + 2, j + 1));
+  }
+
+  return imIn[finalPos[0] - 1][finalPos[1] - 1];
+};
